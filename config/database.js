be0e136d41bc89b7225068b13f9fa5ac5cc3e4f3
Ivan <iv@Ivans-MacBook-Air.local>
2022-08-29 21:14:48 +0300

    db

--- config/database.js
+++ config/database.js
@@ -1,16 +1,15 @@
+const path = require("path");
+
 module.exports = ({ env }) => ({
   connection: {
-    client: "postgres",
+    client: "sqlite",
     connection: {
-      host: env("DATABASE_HOST"),
-      port: env.int("DATABASE_PORT"),
-      database: env("DATABASE_NAME"),
-      user: env("DATABASE_USERNAME"),
-      password: env("DATABASE_PASSWORD"),
-      ssl: {
-        ca: env("DATABASE_CA"),
-      },
+      filename: path.join(
+        __dirname,
+        "..",
+        env("DATABASE_FILENAME", ".tmp/data.db")
+      ),
     },
-    debug: false,
+    useNullAsDefault: true,
   },
 });
